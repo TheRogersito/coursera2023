3f3a2040cffa7291ad0ac6c971e66fd093bf6a3a
TheRogersito <rogguicam@gmail.com>
2023-09-07 22:21:22 +0200

    tres en ratlla

---
+++ tresenratlla/script.js
@@ -0,0 +1,32 @@
+let torn;
+let compro = {};
+let mark = function(ere){
+    if(torn){
+        document.getElementById(ere).innerHTML = '<img class="imatge" src="mare.jpg">';
+        torn = false;
+        compro[ere] = 1;
+    }
+    else{
+        document.getElementById(ere).innerHTML = '<img class="imatge" src="morgana.jpg">';
+        torn = true;
+        compro[ere] = 0;
+    }
+    indicaTorn();
+    setTimeout(comproWin,10)
+}
+let indicaTorn = function(){
+    if(torn){
+        document.getElementById('tornde').innerHTML = '<img class="imatge" id="imatgeTorn" src="mare.jpg">';
+    }
+    else{
+        document.getElementById('tornde').innerHTML = '<img class="imatge" id="imatgeTorn" src="morgana.jpg">';
+    }
+}
+let comproWin = function(){
+    if(compro.box1 == 0 && compro.box2 == 0 && compro.box3 == 0 || compro.box4 == 0 && compro.box5 == 0 && compro.box6 == 0 || compro.box7 == 0 && compro.box8 == 0 && compro.box9 == 0 || compro.box1 == 0 && compro.box4 == 0 && compro.box7 == 0 || compro.box2 == 0 && compro.box5 == 0 && compro.box8 == 0 || compro.box3 == 0 && compro.box6 == 0 && compro.box9 == 0 || compro.box1 == 0 && compro.box5 == 0 && compro.box9 == 0 || compro.box3 == 0 && compro.box5 == 0 && compro.box7 == 0){
+        alert("Ha guanyat el senyoret content");
+    }
+    if(compro.box1 == 1 && compro.box2 == 1 && compro.box3 == 1 || compro.box4 == 1 && compro.box5 == 1 && compro.box6 == 1 || compro.box7 == 1 && compro.box8 == 1 && compro.box9 == 1 || compro.box1 == 1 && compro.box4 == 1 && compro.box7 == 1 || compro.box2 == 1 && compro.box5 == 1 && compro.box8 == 1 || compro.box3 == 1 && compro.box6 == 1 && compro.box9 == 1 || compro.box1 == 1 && compro.box5 == 1 && compro.box9 == 1 || compro.box3 == 1 && compro.box5 == 1 && compro.box7 == 1){
+        alert("Ha guanyat el gatet content");
+    }
+}
