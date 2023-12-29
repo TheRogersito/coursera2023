5c2a3e30bdf4329478fffe5cec0acc3f59b5756d
TheRogersito <rogguicam@gmail.com>
2023-12-29 13:27:00 +0100

    29/12

--- Frontend/React Bàsic/Exemple React 1/firstapp/src/App.js
+++ Frontend/React Bàsic/Exemple React 1/firstapp/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from "./Card.js";
 
@@ -8,8 +7,8 @@ function App() {
       <h1>Task: Add three Card elements</h1>
       <Card h2="Primer h2: Mala puta em deus pasta" h3="Primer h3: No tinc pasta curt de gambals"/>
       <Card h2="Segon h2: No m'agrada la mermelada" h3="Segon h3: Les costelles estan mal fetes"/>
-      <Card h2="Tercer h2: Poca-solta en estan robant" h3="Tercer h3: Són enriquidors culturals"/>
-      <img src={logo} alt="calla"></img>
+      <Card h2="Tercer h2: Poca-solta ens estan robant" h3="Tercer h3: Són enriquidors culturals"/>
+      <img src={require("./Assets/jojo.png") } alt="jojo"/>
     </div>
   );
 }
