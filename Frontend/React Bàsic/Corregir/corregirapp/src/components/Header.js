b95c4364f9d19511f85ad87306b113c31d654758
TheRogersito <rogguicam@gmail.com>
2024-01-30 12:04:02 +0100

    30/1

---
+++ Frontend/React Bàsic/Corregir/corregirapp/src/components/Header.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
+];
+
+const Header = () => {
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) { 
+      setShow(true); 
+    } else { 
+      setShow(false);  
+    }
+    setLastScrollY(window.scrollY); 
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+
+    return () => {
+       window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+      className={`active ${show && 'hidden'}`}
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+              <IconList />
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              <a href="#projects" onClick={handleClick('projects')}>Projects</a>
+              <a href="#contactme" onClick={handleClick('contactme')}>Contact me</a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
+
+function IconList() {
+  const filteredList = socials.map(social => {
+    const filterSocial =
+        <a href={social.url}>
+          <FontAwesomeIcon icon={social.icon} size="2x"/>
+        </a>
+    return <span spacing={8}>{filterSocial}</span>
+  })
+  return (
+    <HStack spacing={8}>{filteredList}</HStack>
+  )
+}
